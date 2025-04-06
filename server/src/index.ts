2e4349b6c2ceda719abae876ee23d9db19a7b9be
emreSlim <emraanqureshi9@gmail.com>
2025-04-06 12:28:26 +0530

    added testing fixture

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,13 +7,14 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+const testing = false;
+
 let generateSuggestion: (input: string) => Promise<string | null>;
 if (process.env.USE_COHEREAI === "true") {
   generateSuggestion = cohereAIMethod;
 } else {
   generateSuggestion = openAIMethod;
 }
-
 
 const app = express();
 const PORT = 3000;
@@ -35,19 +36,23 @@ app.post("/suggest", async (req: Request, res: Response) => {
 
   console.log("Received input:", input);
 
-  //wait for 5 second before sending the response
-  // await new Promise((resolve) => setTimeout(resolve, 5000));
+  if (testing) {
+    //wait for 5 second before sending the response
+    await new Promise((resolve) => setTimeout(resolve, 5000));
 
-    // res.json({ suggestion: "that I've been working on. It's designed to streamline your writing process by predicting and suggesting the next words or phrases as you type, helping you write faster and with fewer errors." });
+    res.json({
+      suggestion:
+        "that I've been working on. It's designed to streamline your writing process by predicting and suggesting the next words or phrases as you type, helping you write faster and with fewer errors.",
+    });
+  } else {
+    if (!input || typeof input !== "string") {
+      res.status(400).json({ error: "Invalid input" });
+      return;
+    }
 
-
-  if (!input || typeof input !== "string") {
-    res.status(400).json({ error: "Invalid input" });
-    return;
+    const suggestion = await generateSuggestion(input);
+    res.json({ suggestion });
   }
-
-  const suggestion = await generateSuggestion(input);
-  res.json({ suggestion });
 });
 
 app.listen(PORT, () => {
